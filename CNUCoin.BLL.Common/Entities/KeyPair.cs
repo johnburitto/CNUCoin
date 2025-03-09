42eebeff24d7c3329aefdd620da8156a4fb52bed
johnburitto <79087305+johnburitto@users.noreply.github.com>
2025-03-10 00:59:23 +0200

    Add docker-compose fiel and BLL entities

---
+++ CNUCoin.BLL.Common/Entities/KeyPair.cs
@@ -0,0 +1,23 @@
+﻿namespace CNUCoin.BLL.Common.Entities
+{
+	/// <summary>
+	/// Holds information about <see cref="Member"/> key pair.
+	/// </summary>
+	public class KeyPair
+	{
+		/// <summary>
+		/// Provides information about key pair id.
+		/// </summary>
+		public Guid KeyPairId { get; set; }
+
+		/// <summary>
+		/// Provides information about public key.
+		/// </summary>
+		public string? PublicKey { get; set; }
+
+		/// <summary>
+		/// Provides information about private key.
+		/// </summary>
+		public string? PrivateKey { get; set; }
+	}
+}
---
+++ CNUCoin.BLL.Common/Entities/Wallet.cs
@@ -0,0 +1,33 @@
+﻿namespace CNUCoin.BLL.Common.Entities
+{
+	/// <summary>
+	/// Holds information about <see cref="Member"/> wallet.
+	/// </summary>
+	public class Wallet
+	{
+		/// <summary>
+		/// Provides information about wallet id.
+		/// </summary>
+		public Guid WalletId { get; set; }
+
+		/// <summary>
+		/// Provides information about transaction date.
+		/// </summary>
+		public DateTime TransactionDate { get; set; }
+
+		/// <summary>
+		/// Provides information about sender.
+		/// </summary>
+		public Guid From {  get; set; }
+		
+		/// <summary>
+		/// Provides information about receiver.
+		/// </summary>
+		public Guid To { get; set; }
+
+		/// <summary>
+		/// Provides information about transaction amount.
+		/// </summary>
+		public int Amount { get; set; }
+	}
+}
